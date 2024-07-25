12d623764e964f1140b6e066a01520c0330a431b
yeonnan <145152442+yeonnan@users.noreply.github.com>
2024-07-25 22:09:28 +0900

    Posts 게시글 생성 2024.07.25 1. posts 게시글 생성

--- posts/views.py
+++ posts/views.py
@@ -4,6 +4,7 @@ from posts.models import Post, Comment
 from posts.serializers import PostSerializer
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 from rest_framework.response import Response
+import uuid
 
 
 class PostList(APIView):
@@ -11,10 +12,17 @@ class PostList(APIView):
 
     # 게시판 조회
     def get(self, request):
+
+
         post = Post.objects.all()
         serializer = PostSerializer(post, many=True)
         return Response(serializer.data)
 
     # 게시글 생성
     def post(self, request):
-        pass
+        # 이미지 이름을 유효 아이디로 해싱
+        request.data.image = f'accounts_{uuid.uuid4()}.png'
+        serializer = PostSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response(serializer.data, status=200)

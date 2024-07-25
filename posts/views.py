840ddae23f73bf25b353b097ae09003e9cee677c
yeonnan <145152442+yeonnan@users.noreply.github.com>
2024-07-25 21:07:26 +0900

    Posts 게시글 조회 2024.07.25 1. posts 게시글 조회

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from posts.models import Post, Comment
+from posts.serializers import PostSerializer
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
+from rest_framework.response import Response
 
-# Create your views here.
+
+class PostList(APIView):
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+    # 게시판 조회
+    def get(self, request):
+        post = Post.objects.all()
+        serializer = PostSerializer(post, many=True)
+        return Response(serializer.data)
+
+    # 게시글 생성
+    def post(self, request):
+        pass

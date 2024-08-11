3f741ac466c74ef79a5c7231402c184a2133494e
yeonnan <145152442+yeonnan@users.noreply.github.com>
2024-08-11 22:42:56 +0900

    kakao rest api key

--- maps/views.py
+++ maps/views.py
@@ -1,8 +1,21 @@
+import requests
 from django.shortcuts import render, get_object_or_404
-from . import config
+from LocalSharing.config import KAKAO_REST_API_KEY
 from rest_framework.views import APIView
 from maps.models import Map, MapLikeUser
 from maps.serializers import MapSerializer, MapLikeUserSerializer
+
+
+def kakao_rest_api(longitude, latitude):
+    url = 'https://dapi.kakao.com/v2/local/geo/coord2regioncode.json'
+    headers = {'Authorization': f'KakaoAK {KAKAO_REST_API_KEY}'}
+    params = {'x': longitude, 'y': latitude}
+
+    response = requests.get(url, headers=headers, params=params)
+    if response.status_code == 200:
+        return response.json()
+    else:
+        return None
 
 
 class MapListAPIView(APIView):

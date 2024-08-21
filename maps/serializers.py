e42c1881f3527914d16c201b9dcb8cae00585028
yeonnan <145152442+yeonnan@users.noreply.github.com>
2024-08-21 23:43:53 +0900

    Maps 저장 로직 24.08.21 1. 선택된 장소의 Map model에 저장 후, 해당 장소에 대한 좋아요 기능 추가 2. 좋아요와 좋아요 취소 로직 분리

--- maps/serializers.py
+++ maps/serializers.py
@@ -13,42 +13,20 @@ class MapSearchSerializer(serializers.Serializer):
     region = serializers.CharField(max_length = 100)
     weather = serializers.CharField(max_length=50)
     category = serializers.CharField(max_length=50, required=False)
-    longitude = serializers.FloatField()
-    latitude = serializers.FloatField()
+    longitude = serializers.FloatField(required=False)
+    latitude = serializers.FloatField(required=False)
 
 
 class MapLikeUserSerializer(serializers.ModelSerializer):
-    user_id = serializers.PrimaryKeyRelatedField(queryset=MapLikeUser.objects.all())
-    map_id = serializers.PrimaryKeyRelatedField(queryset=Map.objects.all())
-    
     class Meta:
         model = MapLikeUser
         fields = '__all__'
+        read_only_fields = ['user']
 
     def create(self, validated_data):
         user = self.context['request'].user
-        title = self.context['request'].data.get('title')
-        url = self.context['request'].data.get('url')
-        region = self.context['request'].date.get('region')
-        weather = self.context['request'].data.get('weather')
-        category = self.context['request'].data.get('category')
+        map_instance = validated_data.get('map')
 
-        # 좋아요 누른 게시글 정보 map에 가져오거나 저장
-        map_instance, created = Map.objects.get_or_create(
-            title = title,
-            url = url,
-            region = region,
-            weather = weather,
-            category = category
-        )
+        # 좋아요를 추가하는 경우에만 create 호출
+        return MapLikeUser.objects.create(user=user, map=map_instance)
 
-        # 좋아요 상태 확인, 처리
-        like, created = MapLikeUser.objects.get_or_create(
-            user = user,
-            map = map_instance
-        )
-
-        if not created:
-            like.delete()
-            return None # 좋아요 취소
-        return like # 좋아요 추가
--- maps/urls.py
+++ maps/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.MapListAPIView.as_view(), name='map-list'),
     path('<int:map_id>/like/', views.MapLikeAPIView.as_view(), name='map-like'),
+    path('save/', views.MapSaveAPIView.as_view(), name='map-create'),
 ]
